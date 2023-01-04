b2750523dc440375d28c5c06ea6207738f7b5fc4
Patrick Dolan <dolanp1992@gmail.com>
2023-01-04 14:55:06 -0800

    update: add selectPlan and selectAddons methods to order class

--- js/Order.js
+++ js/Order.js
@@ -7,4 +7,66 @@ export class Order {
     this.addons = [];
     this.total = 0;
   }
+
+  selectPlan(plan, planFrequency) {
+    switch (plan) {
+      case plan:
+        this.plan = {
+          name: plan,
+          price: 9,
+          planFrequency: planFrequency
+        }
+        break;
+      case plan:
+        this.plan = {
+          name: plan,
+          price: 12,
+          planFrequency: planFrequency
+        }
+        break;
+      case "Pro":
+        this.plan = {
+          name: plan,
+          price: 15,
+          planFrequency: planFrequency
+        }
+        break;
+      default:
+        this.plan = {
+          name: "Arcade",
+          price: 9,
+          planFrequency: planFrequency
+        }
+    }
+  }
+
+  selectAddons(selectedAddons) {
+    if (selectedAddons.length < 1) return
+    // this.addons = selectedAddons;
+    selectedAddons.forEach(element => {
+      switch (element) {
+        case "Online service":
+          this.addons = [...this.addons, {
+            name: "Online service",
+            details: "Access to multiplayer games",
+            price: 1
+          }]
+        break;
+        case "Larger storage":
+          this.addons = [...this.addons, {
+            name: "Larger storage",
+            details: "Extra 1TB of cloud save",
+            price: 2
+          }]
+        break;
+        case "Customizable profile":
+          this.addons = [...this.addons, {
+            name: "Customizable profile",
+            details: "Custom theme on your profile",
+            price: 2
+          }]
+        break;
+      }
+    });
+  }
 }
